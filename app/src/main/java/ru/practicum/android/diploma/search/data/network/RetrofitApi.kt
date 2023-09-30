fc8702dce1d127d4fa9350f7a0371998bc67d5cf
deiteriy <komarov.dmit@gmail.com>
2023-10-01 01:25:25 +0400

    feat: добавлен запрос запрос всех вакансий

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/RetrofitApi.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/network/RetrofitApi.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.diploma.search.data.network
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Path
+import retrofit2.http.QueryMap
 
 interface RetrofitApi {
 
@@ -10,10 +11,13 @@ interface RetrofitApi {
         "Authorization: Bearer YOUR_TOKEN",
         "HH-User-Agent: Application Name (valhero@yandex.ru)"
     )
+
     @GET("/vacancies/{vacancy_id}")
     suspend fun getVacancy(@Path("vacancy_id") id: String): Response
 
     @GET("/vacancies/{vacancy_id}/similar_vacancies")
     suspend fun getSimilarVacancies(@Path("vacancy_id") id: String): Response
 
+    @GET("/vacancies")
+    suspend fun getVacanciesWithFilter(@QueryMap filters:Map<String, String>): Response
 }
