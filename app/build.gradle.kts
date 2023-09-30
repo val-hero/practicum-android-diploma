eb34da3776cdede4a67b0712f863e5dd4427ea99
deiteriy <komarov.dmit@gmail.com>
2023-09-30 19:37:19 +0400

    feat: добавлена библиотека fragment и navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    val fragment_version = "1.6.1"
+    implementation("androidx.fragment:fragment-ktx:$fragment_version")
+    val nav_version = "2.7.3"
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 }
