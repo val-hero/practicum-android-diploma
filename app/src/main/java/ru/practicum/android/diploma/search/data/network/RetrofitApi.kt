a4ddbee7def2ea6033231932cbf64a623d75d261
deiteriy <komarov.dmit@gmail.com>
2023-10-01 01:06:27 +0400

    feat: добавлен запрос в RetrofitApi по id вакансии из поиска, добавлен open class Response

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/RetrofitApi.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/network/RetrofitApi.kt
@@ -1,4 +1,16 @@
 package ru.practicum.android.diploma.search.data.network
 
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+
 interface RetrofitApi {
+
+    @Headers(
+        "Authorization: Bearer YOUR_TOKEN",
+        "HH-User-Agent: Application Name (valhero@yandex.ru)"
+    )
+    @GET("/vacancies/{vacancy_id}")
+    suspend fun getVacancy(@Path("vacancy_id") id: String): Response
+
 }
