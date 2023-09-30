f1e59cddb1c0551fc8c206c5167dcacf56dfedf3
deiteriy <komarov.dmit@gmail.com>
2023-09-30 19:59:49 +0400

    feat: добавлены navHostFragment и navContoller в root activity

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -2,6 +2,7 @@ package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 //import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
 
@@ -9,6 +10,9 @@ class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+        val navController = navHostFragment.navController
 
         // Пример использования access token для HeadHunter API
      //   networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
