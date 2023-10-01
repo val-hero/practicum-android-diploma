d730a62326605fafb875ea1af3ddd28ff9c52edb
Bivis <lumenfan63@mail.ru>
2023-10-01 22:02:55 +0400

    feat: сделал adapter и viewHolder для выбора страны

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/SelectCountry/SelectCountryFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/SelectCountry/SelectCountryFragment.kt
@@ -5,19 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentSelectCountryBinding
 
 class SelectCountryFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentSelectCountryBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var adapter: SelectCountryAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_select_country, container, false)
+    ): View{
+        _binding = FragmentSelectCountryBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val countries = arrayOf("Россия", "Украина", "Казахстан", "Азербайджан", "Беларусь", "Грузия", "Кыргыстан", "Узбекистан", "Другие регионы")
+        adapter= SelectCountryAdapter(countries)
+
+    }
 }
