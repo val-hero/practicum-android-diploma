0b493bd09e07cefb8a79c84043461a0355dfe5a4
NadezhdaSakal <sakaln88@gmail.com>
2023-10-02 08:34:12 +0300

    feat: создала стэйт экрана поиска

---
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/state/SearchScreenState.kt
@@ -0,0 +1,19 @@
+package ru.practicum.android.diploma.search.ui.state
+
+import ru.practicum.android.diploma.search.domain.models.Vacancy
+
+sealed interface SearchScreenState {
+
+    object Loading : SearchScreenState
+
+    object NothingFound : SearchScreenState
+
+    data class Success (
+    val vacancies: List<Vacancy>
+    ) : SearchScreenState
+
+    data class Error(
+        val error: Int?
+    ) : SearchScreenState
+
+}
