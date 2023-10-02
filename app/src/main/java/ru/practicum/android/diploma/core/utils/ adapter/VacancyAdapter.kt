74c841829ef222e24a5714c4fd764d08f1f906ed
NadezhdaSakal <sakaln88@gmail.com>
2023-10-02 21:05:17 +0300

    refactor: изменила on Clic, добавила binding

--- app/src/main/java/ru/practicum/android/diploma/core/utils/ adapter/VacancyAdapter.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/utils/ adapter/VacancyAdapter.kt
@@ -3,22 +3,28 @@ package ru.practicum.android.diploma.core.utils.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ItemSearchBinding
 import ru.practicum.android.diploma.search.domain.models.Vacancy
 
 
-class VacancyAdapter(private val vacancies: ArrayList<Vacancy>) :
+class VacancyAdapter(
+    private val onClick: (Vacancy) -> Unit,
+    private val onLongClick: (Vacancy) -> Boolean
+) :
     RecyclerView.Adapter<VacancyViewHolder>() {
 
-    var itemClickListener: ((Int, Vacancy) -> Unit)? = null
-    var itemLongClickListener: ((Int, Vacancy) -> Unit)? = null
+    val vacancies = ArrayList<Vacancy>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VacancyViewHolder {
-        return VacancyViewHolder(parent)
+        val layoutInflater = LayoutInflater.from(parent.context)
+
+        return VacancyViewHolder(
+            ItemSearchBinding.inflate(layoutInflater, parent, false),
+            onClick, onLongClick
+        )
     }
 
     override fun getItemCount(): Int {
@@ -26,16 +32,7 @@ class VacancyAdapter(private val vacancies: ArrayList<Vacancy>) :
     }
 
     override fun onBindViewHolder(holder: VacancyViewHolder, position: Int) {
-        val vacancy = vacancies[position]
-        holder.bind(vacancy)
-        holder.itemView.setOnClickListener() {
-            itemClickListener?.invoke(position, vacancy)
-        }
-
-        holder.itemView.setOnLongClickListener(){
-            itemLongClickListener?.invoke(position, vacancy)
-            return@setOnLongClickListener true
-        }
+        holder.bind(vacancies[position])
     }
 
     fun setVacancies(newVacancy: List<Vacancy>?) {
@@ -47,24 +44,25 @@ class VacancyAdapter(private val vacancies: ArrayList<Vacancy>) :
     }
 }
 
-class VacancyViewHolder(parentView: ViewGroup) : RecyclerView.ViewHolder(
-    LayoutInflater.from(parentView.context)
-        .inflate(R.layout.item_search, parentView, false)
-) {
-    private val logo: ImageView = itemView.findViewById(R.id.image)
-    private val name: TextView = itemView.findViewById(R.id.title)
-    private val employerName: TextView = itemView.findViewById(R.id.company)
-    private val salary: TextView = itemView.findViewById(R.id.salary)
+class VacancyViewHolder(
+    private val binding: ItemSearchBinding,
+    private val onClick: (Vacancy) -> Unit,
+    private val onLongClick: (Vacancy) -> Boolean
+) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(model: Vacancy) {
-        name.text = "${model.name}, ${model.area?.name}"
-        employerName.text = model.employer?.name.toString()
-        salary.text = getSalary(model, salary.context)
+        with(binding) {
+            title.text = "${model.name}, ${model.area?.name}"
+            company.text = model.employer?.name.toString()
+            salary.text = getSalary(model, salary.context)
+            root.setOnClickListener { onClick(model) }
+            root.setOnLongClickListener { onLongClick(model) }
 
-        Glide.with(itemView)
-            .load(model.employer?.logoUrls?.smallLogo)
-            .placeholder(R.drawable.placeholder)
-            .into(logo)
+            Glide.with(itemView)
+                .load(model.employer?.logoUrls?.smallLogo)
+                .placeholder(R.drawable.placeholder)
+                .into(logo)
+        }
     }
 
     private fun getSalary(model: Vacancy, context: Context): String {
