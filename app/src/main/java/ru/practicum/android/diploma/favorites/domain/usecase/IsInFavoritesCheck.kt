278a6c649181b36922729793121f95b43c85b7af
NadezhdaSakal <sakaln88@gmail.com>
2023-10-05 14:27:20 +0300

    refactor: rename usecase

--- app/src/main/java/ru/practicum/android/diploma/favorites/domain/usecase/IsInFavoritesCheck.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/domain/usecase/IsInFavoritesCheck.kt
@@ -3,7 +3,7 @@ package ru.practicum.android.diploma.favorites.domain.usecase
 import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.favorites.domain.repository.FavoritesRepository
 
-class IsInFavoriteCheck(private val repo: FavoritesRepository) {
+class IsInFavoritesCheck(private val repo: FavoritesRepository) {
 
     fun checkFavorites(id:Long) : Flow<Boolean> {
         return repo.isFavorite(id)
