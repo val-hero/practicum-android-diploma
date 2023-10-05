a9e2274fb4fb2138120859f99a56932811686a78
Bivis <lumenfan63@mail.ru>
2023-10-06 00:47:38 +0400

    feat: добавил экстеншены в koin

--- app/src/main/java/ru/practicum/android/diploma/core/di/FilterModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/di/FilterModule.kt
@@ -1,14 +1,19 @@
 package ru.practicum.android.diploma.core.di
 
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
+import ru.practicum.android.diploma.filter.data.impl.AreasRepositoryImpl
 import ru.practicum.android.diploma.filter.data.impl.CountryRepositoryImpl
+import ru.practicum.android.diploma.filter.domain.AreasRepository
 import ru.practicum.android.diploma.filter.domain.CountryRepository
+import ru.practicum.android.diploma.filter.domain.usecase.GetAreasUseCase
 import ru.practicum.android.diploma.filter.domain.usecase.GetCountriesUseCase
 import ru.practicum.android.diploma.filter.ui.SelectCountry.viewmodel.SelectCountryViewModel
+import ru.practicum.android.diploma.filter.ui.selectregion.viewmodel.SelectRegionViewModel
 
 val filterModule = module {
 
@@ -17,4 +22,12 @@ val filterModule = module {
     factoryOf(::GetCountriesUseCase).bind<GetCountriesUseCase>()
 
     viewModelOf(::SelectCountryViewModel).bind<SelectCountryViewModel>()
+
+    singleOf(::AreasRepositoryImpl).bind<AreasRepository>()
+
+    factoryOf(::GetAreasUseCase).bind<GetAreasUseCase>()
+
+    viewModelOf(::SelectRegionViewModel).bind()
+
+
 }
