32b4bd0275c9d8f3210e80626e87bf43cee56128
Bivis <lumenfan63@mail.ru>
2023-10-05 22:43:38 +0400

    feat: Подготовил SelectIndustryFragment для отображения списка отраслей

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/selectindustry/SelectIndustryFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/selectindustry/SelectIndustryFragment.kt
@@ -4,23 +4,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import ru.practicum.android.diploma.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import ru.practicum.android.diploma.databinding.FragmentSelectIndustryBinding
+import ru.practicum.android.diploma.filter.IndustryAndRegionSelectorAdapter
 
 
 class SelectIndustryFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var _binding: FragmentSelectIndustryBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var adapter: IndustryAndRegionSelectorAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_select_industry, container, false)
+    ): View {
+        _binding = FragmentSelectIndustryBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding.industryRecycler.layoutManager = LinearLayoutManager(requireContext())
+        adapter = IndustryAndRegionSelectorAdapter(emptyList(), ::onIndustryClick)
+        binding.industryRecycler.adapter = adapter
+    }
+
+    private fun onIndustryClick(industryName: String) {
+        Toast.makeText(requireContext(), "Клац", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
