3671d5558a72c88714af5c1cb9274749dcdbaadf
NadezhdaSakal <sakaln88@gmail.com>
2023-10-06 10:53:55 +0300

    feat: добавила вьюмодель в модуль

---
+++ app/src/main/java/ru/practicum/android/diploma/favorites/ui/viewmodel/FavoritesFragmentViewModel.kt
@@ -0,0 +1,8 @@
+package ru.practicum.android.diploma.favorites.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import ru.practicum.android.diploma.favorites.domain.usecase.AddToFavorites
+
+class FavoritesFragmentViewModel(private val addToFavoritesUseCase: AddToFavorites) : ViewModel() {
+
+}
