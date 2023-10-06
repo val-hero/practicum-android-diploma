125aebf0c806c873a4d10dbaa2f3ebd0a997c714
NadezhdaSakal <sakaln88@gmail.com>
2023-10-06 11:02:56 +0300

    feat: создала вьюмодель избранного

--- app/src/main/java/ru/practicum/android/diploma/favorites/ui/viewmodel/FavoritesFragmentViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/ui/viewmodel/FavoritesFragmentViewModel.kt
@@ -1,8 +1,60 @@
 package ru.practicum.android.diploma.favorites.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import ru.practicum.android.diploma.favorites.domain.usecase.AddToFavorites
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import ru.practicum.android.diploma.core.utils.Constants
+import ru.practicum.android.diploma.core.utils.debounce
+import ru.practicum.android.diploma.favorites.domain.usecase.GetFavorites
+import ru.practicum.android.diploma.favorites.ui.state.FavoritesScreenState
+import ru.practicum.android.diploma.search.domain.models.Vacancy
 
-class FavoritesFragmentViewModel(private val addToFavoritesUseCase: AddToFavorites) : ViewModel() {
+class FavoritesFragmentViewModel(
+    private val getFavoritesuseCase: GetFavorites
+) : ViewModel() {
 
+    private val contentStateLiveData = MutableLiveData<FavoritesScreenState>()
+    fun observeContentState(): LiveData<FavoritesScreenState> = contentStateLiveData
+
+    var isClickable = true
+
+    private val vacancyClickDebounce =
+        debounce<Boolean>(Constants.CLICK_DEBOUNCE_DELAY_MILLIS, viewModelScope, false) {
+            isClickable = it
+        }
+
+    init {
+        getFavoritesVacancies()
+    }
+
+    fun getFavoritesVacancies() {
+        viewModelScope.launch {
+            getFavoritesuseCase
+                .invoke()
+                .collect { favoritesVacancies ->
+                    processResult(favoritesVacancies)
+                }
+        }
+    }
+
+    private fun processResult(vacanciesList: List<Vacancy>) {
+        when {
+            vacanciesList.isEmpty() -> {
+                contentStateLiveData.value = FavoritesScreenState.Empty
+            }
+
+            else -> {
+                contentStateLiveData.value = FavoritesScreenState.FavoritesVacancies(vacanciesList)
+            }
+        }
+    }
+
+    fun onTrackClick() {
+        isClickable = false
+        vacancyClickDebounce(true)
+    }
 }
+
+
