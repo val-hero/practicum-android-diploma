93263402c6c00582dd62cb11ef9f41df9fb2c189
Bivis <lumenfan63@mail.ru>
2023-10-06 16:51:48 +0400

    feat: создал модель для industry и запрос на получение списка отраслей

---
+++ app/src/main/java/ru/practicum/android/diploma/filter/data/network/dto/feilds/IndustryDto.kt
@@ -0,0 +1,18 @@
+package ru.practicum.android.diploma.filter.data.network.dto.feilds
+
+import ru.practicum.android.diploma.filter.domain.models.fields.Country
+import ru.practicum.android.diploma.filter.domain.models.fields.Industry
+
+data class IndustryDto(
+    val id: String,
+    val name: String,
+    val parentId: String?
+){
+    fun toCountry(): Industry {
+        return Industry(
+            id = this.id,
+            name = this.name,
+            parentId = this.parentId
+        )
+    }
+}
