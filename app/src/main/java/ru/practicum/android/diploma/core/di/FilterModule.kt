30b57b55d219743ce399f9fb7ce5aecf9cec937e
deiteriy <komarov.dmit@gmail.com>
2023-10-07 19:27:46 +0400

    feat: добавлено хранилище для параметров фильтра, созданы методы для изменения полей фильтра

--- app/src/main/java/ru/practicum/android/diploma/core/di/FilterModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/di/FilterModule.kt
@@ -1,16 +1,31 @@
 package ru.practicum.android.diploma.core.di
 
+import android.content.Context
+import com.google.gson.Gson
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
+import ru.practicum.android.diploma.core.utils.Constants.FILTER_PARAMETERS
 import ru.practicum.android.diploma.filter.data.impl.CountryRepositoryImpl
 import ru.practicum.android.diploma.filter.domain.CountryRepository
+import ru.practicum.android.diploma.filter.data.storage.FilterStorageImpl
+import ru.practicum.android.diploma.filter.domain.api.FilterStorage
 import ru.practicum.android.diploma.filter.domain.usecase.GetCountriesUseCase
 import ru.practicum.android.diploma.filter.ui.SelectCountry.viewmodel.SelectCountryViewModel
 
 val filterModule = module {
+
+    factory { Gson() }
+
+    single {
+        androidContext()
+            .getSharedPreferences(FILTER_PARAMETERS, Context.MODE_PRIVATE)
+    }
+
+    singleOf(::FilterStorageImpl).bind<FilterStorage>()
 
     singleOf(::CountryRepositoryImpl).bind<CountryRepository>()
 
@@ -18,3 +33,5 @@ val filterModule = module {
 
     viewModelOf(::SelectCountryViewModel).bind<SelectCountryViewModel>()
 }
+
+
