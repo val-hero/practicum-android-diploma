da6ea919557ff6785141b2154ada6cc35abcbdf7
deiteriy <komarov.dmit@gmail.com>
2023-10-08 22:23:49 +0400

    feat: добавил переход с экрана поиска на экран фильтра по нажатию иконки

--- app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.core.utils.adapter.VacancyAdapter
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 import ru.practicum.android.diploma.search.ui.state.SearchScreenState
@@ -52,10 +54,18 @@ class SearchFragment : Fragment() {
                 SearchScreenState.SearchStringEmpty -> TODO()
             }
         }
+
+        binding?.filterIcon?.setOnClickListener {
+            navToFilter()
+        }
     }
 
     private fun showVacancies() {
         binding?.placeholderImage?.isVisible = false
         binding?.searchRecycler?.isVisible = true
+    }
+
+    private fun navToFilter() {
+        findNavController().navigate(R.id.action_searchFragment_to_filteringSettingsFragment)
     }
 }
