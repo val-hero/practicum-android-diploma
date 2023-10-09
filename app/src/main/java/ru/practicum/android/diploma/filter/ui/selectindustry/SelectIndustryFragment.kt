0d77f8678e58e355b736af015f1a05cd36c7500e
Bivis <lumenfan63@mail.ru>
2023-10-09 18:57:28 +0400

    feat: сделал навигацию между экранами

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/selectindustry/SelectIndustryFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/selectindustry/SelectIndustryFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.core.utils.Resource
@@ -50,6 +51,11 @@ class SelectIndustryFragment : Fragment() {
         binding.industryRecycler.adapter = adapter
 
         viewModel.getIndustry()
+
+        binding.selectIndustryToolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+
     }
 
     private fun onIndustryClick(industryName: String) {
