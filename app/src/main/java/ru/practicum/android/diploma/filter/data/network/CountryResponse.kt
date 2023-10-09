0bfe717996f2fa70293c1a480ef0256a3daeed12
Bivis <lumenfan63@mail.ru>
2023-10-09 21:33:14 +0400

    fix: пофиксил отображение списка стран

--- app/src/main/java/ru/practicum/android/diploma/filter/data/network/CountryResponse.kt
+++
@@ -1,6 +0,0 @@
-package ru.practicum.android.diploma.filter.data.network
-
-import ru.practicum.android.diploma.filter.data.network.dto.fields.CountryDto
-import ru.practicum.android.diploma.search.data.network.api.ApiResponse
-
-class CountryResponse (val result : List<CountryDto>): ApiResponse()
