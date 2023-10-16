fa58d23f114a218d80a12d97861f25b594701602
NadezhdaSakal <sakaln88@gmail.com>
2023-10-16 21:13:41 +0300

    feat: добавила и обработала плейсхолдер не найдено

--- app/src/main/java/ru/practicum/android/diploma/similar_vacancies/ui/fragment/SimilarVacanciesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/similar_vacancies/ui/fragment/SimilarVacanciesFragment.kt
@@ -1,4 +1,4 @@
-package ru.practicum.android.diploma.similar_vacancies.ui
+package ru.practicum.android.diploma.similar_vacancies.ui.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -13,10 +13,12 @@ import ru.practicum.android.diploma.core.utils.ErrorType
 import ru.practicum.android.diploma.core.utils.adapter.VacancyAdapter
 import ru.practicum.android.diploma.databinding.FragmentSimilarVacanciesBinding
 import ru.practicum.android.diploma.search.domain.models.Vacancy
+import ru.practicum.android.diploma.similar_vacancies.ui.state.SimilarVacanciesScreenState
 import ru.practicum.android.diploma.similar_vacancies.ui.viewmodel.SimilarVacanciesViewModel
 
 class SimilarVacanciesFragment : Fragment() {
-    private var binding: FragmentSimilarVacanciesBinding? = null
+    private var _binding: FragmentSimilarVacanciesBinding? = null
+    private val binding get() = _binding!!
     private val viewModel by viewModel<SimilarVacanciesViewModel>()
     private val args by navArgs<SimilarVacanciesFragmentArgs>()
     private val adapter = VacancyAdapter(
@@ -25,27 +27,36 @@ class SimilarVacanciesFragment : Fragment() {
     )
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentSimilarVacanciesBinding.inflate(inflater, container, false)
-        return binding?.root
+        _binding = FragmentSimilarVacanciesBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding?.similarVacanciesRecycler?.adapter = adapter
+        binding.similarVacanciesRecycler.adapter = adapter
 
         viewModel.fetchSimilarVacancies(args.vacancyId)
 
         viewModel.uiState.observe(viewLifecycleOwner) { state ->
             render(state)
         }
+
+        initToolbar()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        binding = null
+        _binding = null
+    }
+
+    private fun initToolbar() {
+        binding.similarVacancyToolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 
     private fun render(screenState: SimilarVacanciesScreenState) {
@@ -66,36 +77,42 @@ class SimilarVacanciesFragment : Fragment() {
 
     private fun showVacancies(vacancies: List<Vacancy>) {
         adapter.setVacancies(vacancies)
-        binding?.similarVacanciesRecycler?.isVisible = true
-        binding?.progressBarForLoad?.isVisible = false
+        binding.similarVacanciesRecycler.isVisible = true
+        binding.progressBarForLoad.isVisible = false
         hideErrors()
     }
 
     private fun hideErrors() {
-        binding?.placeholderServerError?.isVisible = false
-        binding?.placeholderNoInternet?.isVisible = false
+        binding.placeholderServerError.isVisible = false
+        binding.placeholderNoInternet.isVisible = false
+        binding.placeholderNotFound.isVisible = false
     }
 
     private fun showError(type: ErrorType) {
-        binding?.similarVacanciesRecycler?.isVisible = false
-        binding?.progressBarForLoad?.isVisible = false
+        binding.similarVacanciesRecycler.isVisible = false
+        binding.progressBarForLoad.isVisible = false
 
         when (type) {
-            ErrorType.NO_CONNECTION -> binding?.placeholderNoInternet?.isVisible = true
-            else -> binding?.placeholderServerError?.isVisible = true
+            ErrorType.NO_CONNECTION -> binding.placeholderNoInternet.isVisible = true
+            ErrorType.NOT_FOUND -> binding.placeholderNotFound.isVisible = true
+            else -> binding.placeholderServerError.isVisible = true
         }
     }
 
     private fun showLoading() {
         hideErrors()
-        binding?.similarVacanciesRecycler?.isVisible = false
-        binding?.progressBarForLoad?.isVisible = true
+        binding.similarVacanciesRecycler.isVisible = false
+        binding.progressBarForLoad.isVisible = true
     }
 
     private fun onVacancyClick(id: String) {
         if (!viewModel.isClickable) return
 
         viewModel.onVacancyClick()
-        findNavController().navigate(SimilarVacanciesFragmentDirections.actionToVacancyDetailsFragment(id))
+        findNavController().navigate(
+            SimilarVacanciesFragmentDirections.actionToVacancyDetailsFragment(
+                id
+            )
+        )
     }
 }
