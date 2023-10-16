13d5909e936f2b10658f295c1f5f777c4527588d
NadezhdaSakal <sakaln88@gmail.com>
2023-10-16 17:51:25 +0300

    refactor: удалила неиспользуемую константу и импорт

--- app/src/main/java/ru/practicum/android/diploma/search/ui/viewmodel/SearchViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/viewmodel/SearchViewModel.kt
@@ -1,6 +1,5 @@
 package ru.practicum.android.diploma.search.ui.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
