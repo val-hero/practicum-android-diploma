f6f5ab74ac9c429cfce8cd126984bd152c3bb558
Bivis <lumenfan63@mail.ru>
2023-10-24 23:27:27 +0400

    feat : сделал анимацию списка нашей команды

--- app/src/main/java/ru/practicum/android/diploma/team/ui/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/team/ui/TeamFragment.kt
@@ -21,6 +21,18 @@ class TeamFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.developer1.alpha = 0f
+        binding.developer2.alpha = 0f
+        binding.developer3.alpha = 0f
+        binding.developer4.alpha = 0f
+        binding.developer1.animate().alpha(1f).translationYBy((-50).toFloat()).setStartDelay(300).duration=1500
+        binding.developer2.animate().alpha(1f).translationYBy((-50).toFloat()).setStartDelay(300).duration=1500
+        binding.developer3.animate().alpha(1f).translationYBy((-50).toFloat()).setStartDelay(300).duration=1500
+        binding.developer4.animate().alpha(1f).translationYBy((-50).toFloat()).setStartDelay(300).duration=1500
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
